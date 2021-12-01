5b1ef80751065af84ee23bfe3a1d5bde735fdb01
Zac McMillan <84478817+zacmcmillan29@users.noreply.github.com>
2021-12-01 15:51:58 -0700

    good

--- DontOverdose/apps.py
+++ DontOverdose/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DontoverdoseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DontOverdose'
+    name = 'dontoverdose'
