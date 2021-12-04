940ec9823441325b8587c8c18b07cd34520fe3c4
Zac McMillan <84478817+zacmcmillan29@users.noreply.github.com>
2021-12-04 13:02:03 -0700

    more links and changes

--- OpioidAbuse/settings.py
+++ OpioidAbuse/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "intex",
         "USER": "postgres",
-        "PASSWORD": "Hackersstink1",
+        "PASSWORD": "jazz0905",
         "HOST": "localhost",
         "PORT": "5433",
     }
