280fa77eac44b77739432532bba0151d4f79b9b1
EVELYN-RYAN <72516405+EVELYN-RYAN@users.noreply.github.com>
2021-12-04 13:08:27 -0700

    agin

--- OpioidAbuse/settings.py
+++ OpioidAbuse/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "intex",
         "USER": "postgres",
-        "PASSWORD": "Hackersstink1",
+        "PASSWORD": "403",
         "HOST": "localhost",
         "PORT": "5432",
     }
